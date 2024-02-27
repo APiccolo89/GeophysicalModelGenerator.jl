3eed76c59fc33de019167da59df436df1cba0d19
APiccolo89 <85160533+APiccolo89@users.noreply.github.com>
2024-02-27 15:31:39 +0100

    Generation structure trench
    1. Basic definition of the structure
    2. Some place holder for the future (like, how to change some properties along the strike of the trench

---
+++ src/trench.jl
@@ -0,0 +1,23 @@
+# Basic folder where to contain the function of trench
+# Trench structure: structure that contains all the information concerning the trench boundary 
+@with_kw_noshow mutable struct Trench 
+    n_seg_xy =  1     # Number of segment of the trench plane view (for now, 1 segment)
+    A        =  (0.0,0.0)  # Coordinate 1 {set of coordinates}
+    B        =  (0.0,0.0)  # Coordinate 2 {set of coordinates}
+    # Place holder for segment transformation(i.e., for a given segment you can define a function such that curve and bend the coordinate)
+    # Place holder: changing properties along strike: -function to compute the length of the trench
+    # function that compute how much a certain property is changing (i.e., amount of subducted sediment/continent/temperature)
+    # function that encode how much change a certain properties along the strike) 
+    #(in the future should be possible to change the theta max along strike, or the velocity of convergence or the lenght)
+    # = Slab portion 
+    theta_max = 45 # max bending angle, (must be converted into radians)
+    type_bending  = "Ribe"     # Mode Ribe | Linear | Costumize 
+    n_seg         = 50         # definition segment 
+    L0            = 400        # length of the slab
+    D0            = 100        # thickness of the slab 
+end
+
+
+
+
+
